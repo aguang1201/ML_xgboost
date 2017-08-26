53edc92fcb1d9dc4c90dc72aba7c0f976a45af8d
aguang1201 <uubs@yeah.net>
2017-08-27 00:10:47 +0900

    add higgs boson grandient boost

---
+++ kmedoids_test.py
@@ -0,0 +1,52 @@
+from sklearn.metrics.pairwise import pairwise_distances
+import numpy as np
+
+def kMedoids(D, k, tmax=100):
+
+    M = [2, 4]
+    # create a copy of the array of medoid indices
+    Mnew = np.copy(M)
+
+    # initialize a dictionary to represent clusters
+    C = {}
+    for t in range(tmax):
+        # determine clusters, i. e. arrays of data indices
+        J = np.argmin(D[:,M], axis=1)
+        for kappa in range(k):
+            C[kappa] = np.where(J==kappa)[0]
+        # update cluster medoids
+        for kappa in range(k):
+            J = np.mean(D[np.ix_(C[kappa],C[kappa])],axis=1)
+            j = np.argmin(J)
+            Mnew[kappa] = C[kappa][j]
+        np.sort(Mnew)
+        # check for convergence
+        if np.array_equal(M, Mnew):
+            break
+        M = np.copy(Mnew)
+    else:
+        # final update of cluster memberships
+        J = np.argmin(D[:,M], axis=1)
+        for kappa in range(k):
+            C[kappa] = np.where(J==kappa)[0]
+
+    # return results
+    return M, C
+
+data = np.array([[1,4],[-3,2],[1,8],[3,5],[12,4],[3,9],[-5,-2],[4,11],[0,2],[1,-1]])
+
+# distance matrix
+D = pairwise_distances(data, metric='cityblock')
+
+# split into 2 clusters
+M, C = kMedoids(D, 2)
+
+print('centers:')
+for point_idx in M:
+    print( data[point_idx] )
+
+print('')
+print('clustering result:')
+for label in C:
+    for point_idx in C[label]:
+        print('label {0}:　{1}'.format(label, data[point_idx]))
